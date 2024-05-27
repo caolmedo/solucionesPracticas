0e92b7fab36d1f3627d8fd51d750d723f1c2b806
caolmedo <carlota_olmedo@hotmail.com>
2024-05-26 23:23:28 -0300

    Create Ejercicio-2.py

---
+++ Estructura-Condicional/Ejercicio-2.py
@@ -0,0 +1,11 @@
+#Estructura condicional completa (if - else) + Expresión lógica simple
+
+#Saber si una persona es mayor de edad
+
+edad= int(input ("Ingrese su edad: "))
+if edad >= 18:
+     print ("Usted es mayor de edad")
+else:
+     print ("Usted es menor de edad")
+
+#Fin
