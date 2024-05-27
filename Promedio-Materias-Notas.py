333af413da619b61489b7ca3b0e8ffd2e1bee18f
caolmedo <carlota_olmedo@hotmail.com>
2024-05-26 22:15:43 -0300

    Create Promedio-Materias-Notas.py

---
+++ Promedio-Materias-Notas.py
@@ -0,0 +1,15 @@
+print("Calcular el promedio de cinco materias" )
+
+#Se desea conocer el promedio de notas de cinco materias
+#ingreso de notas
+nota1 = float(input("Ingrese la nota de la primera materia: "))
+nota2 = float(input("Ingrese la nota de la segunda materia: "))
+nota3 = float(input("Ingrese la nota de la tercera materia: "))
+nota4 = float(input("Ingrese la nota de la cuarta materia: "))
+nota5 = float(input("Ingrese la nota de la quinta materia: "))
+#calcular el promedio
+promedio= (nota1+nota2+nota3+nota4+nota5)/5
+#imprimir el promedio
+print(f"El promedio de las cinco materias es {promedio:.2f}")
+
+#fin
